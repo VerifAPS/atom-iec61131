a8e1ac9e66c2127d3b94cb324ef35ee61b4a9d1c
Alexander Weigl <weigl@kit.edu>
2018-02-17 04:08:18 +0100

    Try of an atom package for IEC61131.
    * Syntax completed
    * Next: linting
    * Next: test tables

---
+++ spec/iec61131-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import Iec61131View from '../lib/iec61131-view';
+
+describe('Iec61131View', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
